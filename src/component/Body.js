adbb975891fbbeb4a814a2b13ff99f672f7691aa
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-11-20 13:28:29 +0700

    [Edit] PriceLevel

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "../App.css";
 import Images from "../img/shop1.png";
+import CarImage from "../img/car.png";
+import PetImage from "../img/pet.png";
+import BookImage from "../img/booking.png";
 
 class Body extends Component {
   render(props) {
     return (
       <div>
         <div className="row col-12  p-0 m-0 cardbody mt-2">
-          <div className="col-sm-6 col-md-2 px-1 py-1 p-0 m-0">
+          <div className="col-sm-6 col-md-2 p-0 m-0">
             <center>
               <div
                 style={{ backgroundImage: `url(${this.props.picshop})` }}
@@ -16,43 +19,102 @@ class Body extends Component {
             </center>
           </div>
 
-          <div className="col-sm-6 col-md-10 pt-2 pl-5 p-0 m-0">
+          <div className="col-sm-6 col-md-10 pt-3 pl-3 p-0 m-0">
             <div>
               <span className="shopname pr-3">{this.props.shopname}</span>
               <span className=" px-2">{this.props.status}</span>
             </div>
             <div className="pt-2 explainshop">
               {this.props.shoptype}
-              <span className="pl-3">|</span>
-              <span className="pl-3">{this.props.levelprice}</span>
-              <span className="ratelevel">{this.props.ratelevel}</span>
-              <span className="pl-3">|</span>
-              <span className="pl-3">{this.props.province}</span>
-              <span className="pl-3">{this.props.city}</span>
+              <span className="pl-2">|</span>
+
+              <span className="pl-2">
+                        {this.props.levelprice == "1"  ? (
+                        <text className="blackprice">฿<text className="greyprice">฿฿฿</text></text>
+                        
+                      ) : (
+                        <span></span>
+                      )}
+                      {this.props.levelprice == "2"  ? (
+                        <text className="blackprice">฿฿<text className="greyprice">฿฿</text></text>
+                        
+                      ) : (
+                        <span></span>
+                      )}
+                      
+                      {this.props.levelprice == "3"  ? (
+                        <text className="blackprice">฿฿฿<text className="greyprice">฿</text></text>
+                        
+                      ) : (
+                        <span></span>
+                      )}
+                      
+                      {this.props.levelprice == "4"  ? (
+                      
+                        <text className="blackprice">฿฿฿฿</text>
+                      
+                      ) : (
+                        <span></span>
+                      )}
+                      
+                      
+                      
+                      </span>
+                      
+
+              <span className="pl-2">|</span>
+              <span className="pl-2">{this.props.province}</span>
+              <span className="pl-2">{this.props.city}</span>
               <div className="underline"></div>
               <div className="highlight">{this.props.describe}</div>
 
-              <div className="py-2">
+              <div className="py-3">
                 {" "}
                 <span className="recommend">เมนูแนะนำ: </span>
                 {this.props.recommend}
               </div>
-              <div className=" pt-3">
+              <div className="displayflex">
                 {this.props.facilities.map((facilitie) => {
                   return (
-                    <div className="facilities">
-                      {/* <img
-                        src=""
-                        className="facilitiescircle"
-                        width="20px"
-                      ></img> */}
-                      {facilitie}
-                      
+                    <div className="facilities" 
+                    > 
+                      {facilitie == "ที่จอดรถ"  ? (
+                        <img
+                          src={CarImage}
+                          className="facilitiescircle" 
+                          width="18px"
+                        ></img>
+                      ) : (
+                        <div></div>
+                      )}
+                       {facilitie == "สามารถนำสัตว์เลี้ยงเข้าได้"  ? (
+                        <img
+                          src={PetImage}
+                          className="facilitiescircle" 
+                          width="18px"
+                        ></img>
+                      ) : (
+                        <div></div>
+                      )}
+                       {facilitie == "รับจองล่วงหน้า"  ? (
+                        <img
+                          src={BookImage}
+                          className="facilitiescircle" 
+                          width="18px"
+                        ></img>
+                      ) : (
+                        <div></div>
+                      )}
+
+                      {/* {facilitie} */}
                     </div>
+                   
+                   
                   );
                 })}
               </div>
             </div>
+            
           </div>
         </div>
       </div>
