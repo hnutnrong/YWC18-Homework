edf2dead662b6b7e9a1cf9a33a6da4676afdc915
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-11-20 13:27:19 +0700

    [Add] picture facilities

--- src/App.js
+++ src/App.js
@@ -9,20 +9,19 @@ import Price from "./component/Price";
 import LeftCard from "./component/LeftCard";
 import Header from "./component/Header";
 import api from "./utils/api";
+import Seemore from "./component/Seemore";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       shop: [],
-      
     };
   }
   componentDidMount() {
     api.get().then((result) => {
       this.setState({
         shop: result.data.merchants,
-        
       });
     });
   }
@@ -30,55 +29,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <div className="row col-12 m-0 p-0 top ">
-          <div className="col-1 p-0 m-0"></div>
-          <div className="col-10 py-3">
-            <div class="input-group">
-              <img src={Image} width="150px" alt="headlogo"></img>
-              <div className="input-group-prepend pl-4">
-                <button
-                  className="btn btn-outline-secondary dropdown-toggle"
-                  type="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fa fa-map-marker pr-2" aria-hidden="true"></i>
-                  พื้นที่ใกล้ฉัน
-                  <span className="pl-5"></span>
-                </button>
-              </div>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="ค้นหา ชื่อ ร้านอาหาร และเครื่องดื่ม ร้านธงฟ้า ร้านค้า OTOP และสินค้าทั่วไป"
-              />
-              <div class="input-group-append">
-                <button
-                  class="btn btn-outline-secondary"
-                  type="button"
-                  id="button-addon2"
-                >
-                  Button
-                </button>
-              </div>
-            </div>
-
- 
-          </div>
-
-          <div className="col-1"></div>
-        </div> */}
 
         <Header></Header>
-
-        <div className="row col-12 m-0 p-0 title">
-          <div className="col-1 p-0 m-0"></div>
-          <div className="py-2">
-            <span className="topichead pl-3 ">หน้าแรก / ค้นหา</span>
-          </div>
-          <div className="col-2 p-0 m-0"></div>
-        </div>
 
         <div className="pt-4 pb-5 px-2">
           <div className=" pl-3">
@@ -112,10 +64,11 @@ class App extends Component {
                   );
                 })}
               </div>
-
-              <div className="pb-2">
-                
-              </div>
+              <center>
+              <button type="button" className="">
+                <span>ดูเพิ่มเติม</span>
+              </button>
+              </center>
             </div>
           </div>
         </div>
